e6f269079ffba51a297844fc9ad3277c8b0f9ec4
Andaç Lokumcu <andaclokumcu@hotmail.com>
2021-04-20 00:17:33 +0300

    Add project files.

---
+++ Market Values (Logical)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Market_Values__Logical_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double piyasaDun = 115000.6;
+            double piyasaBugun = 116000;
+            bool sonuc = piyasaBugun == piyasaDun;
+
+            string mesajArtis = "Artis oku";
+            string mesajAzalis = "Azalis oku";
+            string mesajAyni = "Sabit oku";
+
+            if(piyasaBugun>piyasaDun)
+            {
+                Console.WriteLine(mesajArtis);
+            }
+            else if(piyasaBugun==piyasaDun)
+            {
+                Console.WriteLine(mesajAyni);
+            }
+            else
+            {
+                Console.WriteLine(mesajAzalis);
+            }
+        }
+    }
+}
