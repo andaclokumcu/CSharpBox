c5dfad004646b117e6f4df3862e54db9403da785
Andaç Lokumcu <andaclokumcu@hotmail.com>
2021-04-21 01:03:32 +0300

    Student List Design

---
+++ Student List/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace Student_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxStudentList = new System.Windows.Forms.ListBox();
+            this.lblStudentList = new System.Windows.Forms.Label();
+            this.lblStudentName = new System.Windows.Forms.Label();
+            this.tbxStudentName = new System.Windows.Forms.TextBox();
+            this.btnAddStudent = new System.Windows.Forms.Button();
+            this.btnRemoveStudent = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbxStudentList
+            // 
+            this.lbxStudentList.FormattingEnabled = true;
+            this.lbxStudentList.ItemHeight = 15;
+            this.lbxStudentList.Location = new System.Drawing.Point(37, 74);
+            this.lbxStudentList.Name = "lbxStudentList";
+            this.lbxStudentList.Size = new System.Drawing.Size(145, 184);
+            this.lbxStudentList.TabIndex = 0;
+            // 
+            // lblStudentList
+            // 
+            this.lblStudentList.AutoSize = true;
+            this.lblStudentList.Location = new System.Drawing.Point(37, 46);
+            this.lblStudentList.Name = "lblStudentList";
+            this.lblStudentList.Size = new System.Drawing.Size(84, 15);
+            this.lblStudentList.TabIndex = 1;
+            this.lblStudentList.Text = "Öğrenci Listesi";
+            // 
+            // lblStudentName
+            // 
+            this.lblStudentName.AutoSize = true;
+            this.lblStudentName.Location = new System.Drawing.Point(251, 74);
+            this.lblStudentName.Name = "lblStudentName";
+            this.lblStudentName.Size = new System.Drawing.Size(73, 15);
+            this.lblStudentName.TabIndex = 2;
+            this.lblStudentName.Text = "Öğrenci Adı:";
+            // 
+            // tbxStudentName
+            // 
+            this.tbxStudentName.Location = new System.Drawing.Point(330, 71);
+            this.tbxStudentName.Name = "tbxStudentName";
+            this.tbxStudentName.Size = new System.Drawing.Size(165, 23);
+            this.tbxStudentName.TabIndex = 3;
+            // 
+            // btnAddStudent
+            // 
+            this.btnAddStudent.Location = new System.Drawing.Point(543, 71);
+            this.btnAddStudent.Name = "btnAddStudent";
+            this.btnAddStudent.Size = new System.Drawing.Size(111, 23);
+            this.btnAddStudent.TabIndex = 4;
+            this.btnAddStudent.Text = "Ekle";
+            this.btnAddStudent.UseVisualStyleBackColor = true;
+            this.btnAddStudent.Click += new System.EventHandler(this.btnAddStudent_Click);
+            // 
+            // btnRemoveStudent
+            // 
+            this.btnRemoveStudent.Location = new System.Drawing.Point(37, 287);
+            this.btnRemoveStudent.Name = "btnRemoveStudent";
+            this.btnRemoveStudent.Size = new System.Drawing.Size(145, 23);
+            this.btnRemoveStudent.TabIndex = 5;
+            this.btnRemoveStudent.Text = "Sil";
+            this.btnRemoveStudent.UseVisualStyleBackColor = true;
+            this.btnRemoveStudent.Click += new System.EventHandler(this.btnRemoveStudent_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRemoveStudent);
+            this.Controls.Add(this.btnAddStudent);
+            this.Controls.Add(this.tbxStudentName);
+            this.Controls.Add(this.lblStudentName);
+            this.Controls.Add(this.lblStudentList);
+            this.Controls.Add(this.lbxStudentList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxStudentList;
+        private System.Windows.Forms.Label lblStudentList;
+        private System.Windows.Forms.Label lblStudentName;
+        private System.Windows.Forms.TextBox tbxStudentName;
+        private System.Windows.Forms.Button btnAddStudent;
+        private System.Windows.Forms.Button btnRemoveStudent;
+    }
+}
+
