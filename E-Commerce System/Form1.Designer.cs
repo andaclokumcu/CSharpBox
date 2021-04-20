7f86f89ffcb2a6200edd70241f5820e84b4f5836
Andaç Lokumcu <andaclokumcu@hotmail.com>
2021-04-21 00:39:05 +0300

    E-Commerce System

---
+++ E-Commerce System/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace E_Commerce_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxProducts = new System.Windows.Forms.ListBox();
+            this.lblProducts = new System.Windows.Forms.Label();
+            this.btnAddToCart = new System.Windows.Forms.Button();
+            this.lbxCart = new System.Windows.Forms.ListBox();
+            this.btnRemoveFromCart = new System.Windows.Forms.Button();
+            this.lblCart = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbxProducts
+            // 
+            this.lbxProducts.FormattingEnabled = true;
+            this.lbxProducts.ItemHeight = 15;
+            this.lbxProducts.Location = new System.Drawing.Point(45, 87);
+            this.lbxProducts.Name = "lbxProducts";
+            this.lbxProducts.Size = new System.Drawing.Size(128, 154);
+            this.lbxProducts.TabIndex = 0;
+            // 
+            // lblProducts
+            // 
+            this.lblProducts.AutoSize = true;
+            this.lblProducts.Location = new System.Drawing.Point(45, 56);
+            this.lblProducts.Name = "lblProducts";
+            this.lblProducts.Size = new System.Drawing.Size(38, 15);
+            this.lblProducts.TabIndex = 1;
+            this.lblProducts.Text = "label1";
+            // 
+            // btnAddToCart
+            // 
+            this.btnAddToCart.Location = new System.Drawing.Point(216, 87);
+            this.btnAddToCart.Name = "btnAddToCart";
+            this.btnAddToCart.Size = new System.Drawing.Size(85, 32);
+            this.btnAddToCart.TabIndex = 2;
+            this.btnAddToCart.Text = "button1";
+            this.btnAddToCart.UseVisualStyleBackColor = true;
+            this.btnAddToCart.Click += new System.EventHandler(this.btnAddToCart_Click);
+            // 
+            // lbxCart
+            // 
+            this.lbxCart.FormattingEnabled = true;
+            this.lbxCart.ItemHeight = 15;
+            this.lbxCart.Location = new System.Drawing.Point(381, 87);
+            this.lbxCart.Name = "lbxCart";
+            this.lbxCart.Size = new System.Drawing.Size(124, 154);
+            this.lbxCart.TabIndex = 3;
+            // 
+            // btnRemoveFromCart
+            // 
+            this.btnRemoveFromCart.Location = new System.Drawing.Point(546, 87);
+            this.btnRemoveFromCart.Name = "btnRemoveFromCart";
+            this.btnRemoveFromCart.Size = new System.Drawing.Size(140, 32);
+            this.btnRemoveFromCart.TabIndex = 4;
+            this.btnRemoveFromCart.Text = "button1";
+            this.btnRemoveFromCart.UseVisualStyleBackColor = true;
+            this.btnRemoveFromCart.Click += new System.EventHandler(this.btnRemoveFromCart_Click);
+            // 
+            // lblCart
+            // 
+            this.lblCart.AutoSize = true;
+            this.lblCart.Location = new System.Drawing.Point(381, 56);
+            this.lblCart.Name = "lblCart";
+            this.lblCart.Size = new System.Drawing.Size(38, 15);
+            this.lblCart.TabIndex = 5;
+            this.lblCart.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCart);
+            this.Controls.Add(this.btnRemoveFromCart);
+            this.Controls.Add(this.lbxCart);
+            this.Controls.Add(this.btnAddToCart);
+            this.Controls.Add(this.lblProducts);
+            this.Controls.Add(this.lbxProducts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxProducts;
+        private System.Windows.Forms.Label lblProducts;
+        private System.Windows.Forms.Button btnAddToCart;
+        private System.Windows.Forms.ListBox lbxCart;
+        private System.Windows.Forms.Button btnRemoveFromCart;
+        private System.Windows.Forms.Label lblCart;
+    }
+}
+
---
+++ E-Commerce System/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace E_Commerce_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string productsText = "Ürünler";
+            string addToCartButtonText = "Sepete Ekle";
+            string cartText = "Sepetiniz";
+            string removeFromCartButtonText = "Sepetten Çıkar";
+
+            lblProducts.Text = productsText;
+            btnAddToCart.Text = addToCartButtonText;
+            lblCart.Text = cartText;
+            btnRemoveFromCart.Text = removeFromCartButtonText;
+
+            string[] products = new string[] { "Laptop", "Masaüstü PC", "Klavye" };
+
+            for (int i = 0; i < products.Length; i++)
+            {
+                lbxProducts.Items.Add(products[i]);
+            }
+        }
+
+        private void btnAddToCart_Click(object sender, EventArgs e)
+        {
+            if (lbxProducts.SelectedItem!=null)
+            {
+                lbxCart.Items.Add(lbxProducts.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Öncelikle bir eleman seçmelisiniz.");
+            }
+        }
+
+        private void btnRemoveFromCart_Click(object sender, EventArgs e)
+        {
+            if (lbxCart.SelectedItem!=null)
+            {
+                lbxCart.Items.Remove(lbxCart.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Öncelikle bir ürün seçmelisiniz.");
+            }
+
+            if (lbxCart.Items.Count==0)
+            {
+                btnRemoveFromCart.Enabled = false;
+            }
+        }
+    }
+}
