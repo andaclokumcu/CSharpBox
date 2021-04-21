4952e9644d98314154ed1bd606dabae8a26a5871
Andaç Lokumcu <andaclokumcu@hotmail.com>
2021-04-21 16:31:13 +0300

    Odd and Even Numbers

---
+++ Odd and Even Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Odd_and_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+
+
+            Console.WriteLine("Sayı giriniz:");
+
+            {
+                for (int i = 1; i < 10; i++)
+                {
+                    
+                    sayi = Convert.ToInt32(Console.ReadLine());
+                   
+                    if (sayi > 0 && sayi % 2 == 0)
+                    {
+                        Console.WriteLine("Sayı çift sayıdır.");
+                    }
+                    if (sayi > 0 && sayi % 2 == 1)
+                    {
+                        Console.WriteLine("Sayı tek sayıdır.");
+                    }
+                }
+            }
+        }
+    }
+}
