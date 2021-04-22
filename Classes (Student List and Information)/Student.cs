fb8c2e4aa2d699ac5a7618c1a83913269ad751f1
Andaç Lokumcu <andaclokumcu@hotmail.com>
2021-04-22 21:19:19 +0300

    Student List and Information (Use of Classes)

---
+++ Classes (Student List and Information)/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes__Student_List_and_Information_
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+        public string Mail { get; set; }
+    }
+}
