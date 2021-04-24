9ca28ff2dcee11a7f10c93088a747887960b7055
Andaç Lokumcu <andaclokumcu@hotmail.com>
2021-04-25 00:16:50 +0300

    {get ; set;}

---
+++ OOP - Properties/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP___Properties
+{
+    class Product
+    {
+        public Product()
+        {
+            _unitPrice = 1000;  //set--> yazdırma
+        }
+
+        decimal _unitPrice;
+
+        public int Id { get; set; }
+        public string ProductNumber { get; set; }
+
+        public decimal UnitPrice
+        {
+            get
+            {
+                return _unitPrice + _unitPrice * 18 / 100; //get--> okuma
+            }
+        }
+
+        public decimal UnitInStock;
+    }
+}
---
+++ OOP - Properties/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OOP___Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Id = 1;
+            Console.WriteLine(product.Id);
+
+            product.UnitInStock = 500;
+            Console.WriteLine(product.UnitInStock);
+
+            
+            Console.WriteLine(product.UnitPrice);
+        }
+    }
+}
