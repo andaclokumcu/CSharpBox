8b0ec2e4097960f847b0d927bc84a9de4f6ff21d
Andaç Lokumcu <andaclokumcu@hotmail.com>
2021-04-30 23:52:53 +0300

    Working with Join Operations and DTO

---
+++ Linq (Join and DTO)/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linq__Join_and_DTO_
+{
+    class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ Linq (Join and DTO)/ProductDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linq__Join_and_DTO_
+{
+    class ProductDto //daha detay
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        //Yukarıdakiler bize lazım olan kriterler
+
+        public string CategoryName { get; set; } 
+        
+//Product sınıfında olmayan bir özellik eklemek istiyoruz.Category sınıfından bir kriter bu. Burada sınıfları join etmemiz gerekiyor.İki classı ilişkilendirmiş olduk.
+    }
+}
