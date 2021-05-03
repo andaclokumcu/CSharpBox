e319e0eecb5f8a03086dc8b77b788b50cd450fc0
Andaç Lokumcu <andaclokumcu@hotmail.com>
2021-05-04 00:19:54 +0300

    Entity Framework Implementation

---
+++ EntityFrameworkDemo/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkDemo
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short UnitsInStock { get; set; }
+
+        //biriminin short olmasının seebebi veritabanındaki dbo.Products tablosundaki UnitsInStock
+        //classının smallint değerine karşılık gelir.
+
+    }
+}
