559a7f64d0b9334cf6c2ebbc95671405345ebe6f
Andaç Lokumcu <andaclokumcu@hotmail.com>
2021-05-07 19:54:15 +0300

    Asynchronous Methods with async await

---
+++ AsyncDemo2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main Thread başladı.{Thread.CurrentThread.ManagedThreadId}");
+            Process1Async();
+            Process2Async();
+            Process1();
+            Process2();
+        }
+
+        static void Process1()
+        {
+            Console.WriteLine($"Senkron işlem 1 başladı.{Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        static async Task Process1Async()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine($"Async işlem 1 başladı.{Thread.CurrentThread.ManagedThreadId}");
+            });
+        }
+
+        static void Process2()
+        {
+            Console.WriteLine($"Senkron işlem 2 başladı.{Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        static async Task Process2Async()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine($"Async işlem 2 başladı.{Thread.CurrentThread.ManagedThreadId}");
+            });
+        }
+    }
+}
