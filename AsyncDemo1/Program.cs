c4c799be23053535668e06e92954272cdceaa2fa
Andaç Lokumcu <andaclokumcu@hotmail.com>
2021-05-07 19:34:46 +0300

    Asynchronous Task System

---
+++ AsyncDemo1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Thread no:{Thread.CurrentThread.ManagedThreadId}");
+
+            Task task1 = new Task(Process1);
+            task1.Start();
+
+            Process2();
+            Console.ReadKey();
+
+        }
+
+        static void Process2()
+        {
+            Console.WriteLine("2.işleme başladım");
+            Console.WriteLine($"Thread no:{Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        static void Process1()
+        {
+            Console.WriteLine("1. işleme başladım");
+            Console.WriteLine($"Thread no:{Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+
+}
